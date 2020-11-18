a2ac1cf3c395ae583031856140f5862eb76b2f27
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-11-18 23:04:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length-1]=="."
   end
 
   def question?
-
+    self[self.length-1]=="?"
   end
 
   def exclamation?
-
+    self[self.length-1]=="!"
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
